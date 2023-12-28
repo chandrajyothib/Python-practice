62d62ccad8caac470d0efd7a2cc27d6d1d6f8efc
chandrajyothib <123759115+chandrajyothib@users.noreply.github.com>
2023-12-28 15:50:29 +0530

    Add files via upload

---
+++ 28_12_2023.py
@@ -0,0 +1,67 @@
+#             typecasting sample codes
+x = "123"
+y = int(2.8)
+z=23.69876
+
+print(x)
+print(y)
+print(z)
+a=float(x)
+b=str(y)
+print(a)
+print(b)
+print(type(b))
+print(len(x))
+
+
+
+#              String methods
+string ="welcome to the world"
+print(string.upper())
+print(string.strip())
+print(string.replace("h", "J"))
+print(string.capitalize())
+print(string.count('o'))
+print(string.islower())
+print(string.isupper())
+print(string.split(" "))
+print(string.title())
+
+
+
+
+
+#          List methods
+list1 = ["apple", "banana", "cherry"]
+print(list1)
+print(len(list1))
+l=list(("apple", "banana", "cherry"))
+print(l)
+l[1]="mango"
+print(l)
+l.append("orange")
+print(l)
+l.insert(5,"guava")
+print(l)
+l.remove("orange")
+print(l)
+l.pop(1)
+print(l)
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+  print(thislist[i])
+
+
+num=[2,1,33,4,55,64,23]
+num.sort()
+
+print(num)
+num.sort(reverse=True)
+print(num)
+l1=["apple", "banana", "cherry","papaya"]
+l2=list((l1))
+print(l1)
+print(l2)
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
